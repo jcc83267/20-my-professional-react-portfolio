b8f26acb5dd7eabff9106b1bce106850bb291fe5
James Canlas <jamescanlasjr@yahoo.com>
2021-05-01 19:32:27 -0700

    navbar layout, can now highlight which part of the nav bar they are viewing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useState } from "react";
 import Nav from './components/Nav';
 import About from './components/About';
 import Resume from './components/Resume';
@@ -7,9 +8,30 @@ import Portfolio from './components/Portfolio';
 
 
 function App() {
+  const [aboutSelected, setAboutSelected] = useState(true);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
+  const [portfolioSelected, setPortfolioSelected] = useState(false);
+
+  const [resumeSelected, setResumeSelected] = useState(false);
+
+
   return (
     <div>
-      <About></About>
+      <Nav
+        aboutSelected={aboutSelected}
+        setAboutSelected={setAboutSelected}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+        portfolioSelected={portfolioSelected}
+        setPortfolioSelected={setPortfolioSelected}
+        resumeSelected={resumeSelected}
+        setResumeSelected={setResumeSelected}
+      ></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, {  } from "react";
 
-function Nav() {
-    return(
-        <div>
+function Nav(props) {
+    const {
+        aboutSelected,
+        setAboutSelected,
+        contactSelected,
+        setContactSelected,
+        portfolioSelected,
+        setPortfolioSelected,
+        resumeSelected,
+        setResumeSelected
 
-        </div>
+    } = props;
+
+    
+    
+
+    return (
+        <header className="flex-row px-1">
+            <h2>
+                <a href="/">
+                    <span role="img" aria-label="portfolio">{" "}📁</span>{" "}James Canlas Jr
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className={`mx-2 ${aboutSelected && 'navActive'}`}>
+                        <span onClick={() => {setAboutSelected(true); setContactSelected(false); setPortfolioSelected(false); setResumeSelected(false)}}>
+                            About Me
+                        </span>
+                    </li>
+                    <li className={`mx-2 ${portfolioSelected && 'navActive'}`}>
+                        <span onClick={() => {setAboutSelected(false); setContactSelected(false); setPortfolioSelected(true); setResumeSelected(false)}}>
+                            Portfolio
+                        </span>
+                    </li>
+                    <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+                        <span onClick={() => {setAboutSelected(false); setContactSelected(true); setPortfolioSelected(false); setResumeSelected(false)}}>
+                            Contact
+                        </span>
+                    </li>
+                    <li className={`mx-2 ${resumeSelected && 'navActive'}`}>
+                        <span onClick={() => {setAboutSelected(false); setContactSelected(false); setPortfolioSelected(false); setResumeSelected(true)}}>
+                            Resume
+                        </span>
+                    </li>
+
+                </ul>
+            </nav>
+        </header>
     );
 }
 
