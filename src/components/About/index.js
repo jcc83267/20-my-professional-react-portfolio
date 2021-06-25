3fc42b4efc3da1e969d61e96fd07267ddb82be18
James Canlas <jamescanlasjr@yahoo.com>
2021-06-24 18:37:59 -0700

    added about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,7 @@ function About() {
       <h1 id="about">About Me?</h1>
       <div className="my-2">
       <p>
-      Will eventually fill out about me.(In progress)  
+        Greetings my name is James Canlas. I am a front end web developer leveraging background in psychology to provide unique perspectives on how end-users interact with websites and software platforms. Earned a certificate in Full Stack Web Development from the University of California of Los Angeles Extension Coding Boot Camp. Innovative problem-solver who is passionate about developing apps with a focus on mobile-first design and development. Strengths in creativity, teamwork, and building projects from ideation to execution.
       </p>
       </div>
     </section>
